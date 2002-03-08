c426072fc90d6ef8ee1d4d896e1c7cdb1e83e0a1
Michael Militzer <isibaar@xvid.org>
2002-03-08 19:54:05 +0000

    This commit was generated by cvs2svn to compensate for changes in r6, which included commits to RCS files with non-trunk default branches.
    git-svn-id: http://svn.xvid.org/trunk@7 060a26f8-e6a2-0410-8959-474133d486e0

---
+++ dshow/src/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by xvid.ax.rc
+//
+#define VERSION_RES_MINOR_VER           0
+#define VERSION_RES_BUILD               0
+#define VER_DEBUG                       0
+#define IDS_ABOUT                       1
+#define VERSION_RES_MAJOR_VER           8
+#define IDD_ABOUT                       102
+#define IDB_LOGO                        103
+#define VERSION_RES_LANGUAGE            0x409
+#define VERSION_RES_CHARSET             1252
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
