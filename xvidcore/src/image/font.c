b01b5f2ce70d691fbdb24d797cb9ad428b561c66
suxen_drol <suxen_drol@060a26f8-e6a2-0410-8959-474133d486e0>
2002-11-03 02:02:44 +0000

    ']' fixup, strlen removed
    git-svn-id: http://svn.xvid.org/branches/dev-api-3@623 060a26f8-e6a2-0410-8959-474133d486e0

--- xvidcore/src/image/font.c
+++ xvidcore/src/image/font.c
@@ -479,9 +479,9 @@ static const char ascii91[6][FONT_WIDTH*FONT_HEIGHT] = {
 		0,0,1,0,
 		0,0,1,0,
 		0,0,1,0,
-		0,1,1,0,
+		0,0,1,0,
+		0,1,1,0 },
 
-		0,0,1,0 },
 	{	0,1,0,1,		// ^
 		0,0,0,0,
 		0,0,0,0,
@@ -532,12 +532,12 @@ void image_printf(IMAGE * img, int edged_width, int height,
 {
 	va_list args;
 	char buf[FONT_BUF_SZ];
-	size_t i;
+	int i;
 
 	va_start(args, fmt);
 	vsprintf(buf, fmt, args);
 
-	for (i = 0; i < strlen(buf); i++) {
+	for (i = 0; i < buf[i]; i++) {
 		const char * font;
 
 		if (buf[i] >= '!' && buf[i] <= '@')
