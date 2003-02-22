a18aa417811af0fb2d242a18a737ee41a6be102e
chl <chl@060a26f8-e6a2-0410-8959-474133d486e0>
2003-02-22 18:19:32 +0000

    Emptied to have space for future implementation
    git-svn-id: http://svn.xvid.org/branches/dev-api-4@891 060a26f8-e6a2-0410-8959-474133d486e0

--- xvidcore/src/motion/smp_motion_est.h
+++ xvidcore/src/motion/smp_motion_est.h
@@ -26,7 +26,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- *  $Id: smp_motion_est.h,v 1.4 2003-02-15 15:22:19 edgomez Exp $
+ *  $Id: smp_motion_est.h,v 1.4.2.1 2003-02-22 18:19:32 chl Exp $
  *
  ***************************************************************************/
 
@@ -35,59 +35,6 @@
 
 #ifdef _SMP
 
-#define MAXNUMTHREADS 16
-
-//#define NUMTHREADS 2
-
-typedef struct 
-{
-
-		MBParam * pParam;
-
-		FRAMEINFO* reference;
-		FRAMEINFO* current;
-		
-		IMAGE* pRef;
-		IMAGE* pRefH;
-		IMAGE* pRefV;
-		IMAGE* pRefHV;
-
-//		int iLimit;		/* currently unused */
-
-		MACROBLOCK * pMBs;
-		MACROBLOCK * prevMBs;
-
-} globaldata;		/* this data is the same for all threads */
-
-typedef struct 
-{
-		int id;
-
-		int minx;
-		int maxx;
-		int miny;
-		int maxy;
-		
-		globaldata *gdata;
-
-} jobdata;			/* every thread get it's personal version of these */
-
-
-void 
-SMP_correct_pmv(int x, int y, int iWcount, MACROBLOCK* pMBs);
-
-void 
-SMP_MotionEstimationWorker(jobdata *arg);
-
-bool
-SMP_MotionEstimation(MBParam * const pParam,
-				 FRAMEINFO * const current,
-				 FRAMEINFO * const reference,
-				 const IMAGE * const pRefH,
-				 const IMAGE * const pRefV,
-				 const IMAGE * const pRefHV,
-				 const uint32_t iLimit);
-
-#endif
+#error SMP support has been removed until B-frame API is stable.
 
 #endif
