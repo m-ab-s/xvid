074d4a5078cac4735e1db718e22004635ebffe60
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2004-07-17 11:42:46 +0000

    Fixes from my tree
    git-svn-id: http://svn.xvid.org/branches/release-1_0-branch@1501 060a26f8-e6a2-0410-8959-474133d486e0

--- xvidcore/examples/xvid_decraw.c
+++ xvidcore/examples/xvid_decraw.c
@@ -20,7 +20,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- * $Id: xvid_decraw.c,v 1.10.2.4 2004-04-15 22:39:30 edgomez Exp $
+ * $Id: xvid_decraw.c,v 1.10.2.5 2004-07-17 11:41:41 edgomez Exp $
  *
  ****************************************************************************/
 
@@ -628,6 +628,10 @@ dec_init(int use_assembler)
 
 	xvid_gbl_init_t   xvid_gbl_init;
 	xvid_dec_create_t xvid_dec_create;
+
+	/* Reset the structure with zeros */
+	memset(&xvid_gbl_init, 0, sizeof(xvid_gbl_init_t));
+	memset(&xvid_dec_create, 0, sizeof(xvid_dec_create_t));
 
 	/*------------------------------------------------------------------------
 	 * XviD core initialization
