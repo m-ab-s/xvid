679f26d8edced43e3fb301359a35ae3b3aaa3040
Isibaar <Isibaar@060a26f8-e6a2-0410-8959-474133d486e0>
2008-11-28 18:28:08 +0000

    adjust version numbers
    git-svn-id: http://svn.xvid.org/branches/release-1_2-branch@1822 060a26f8-e6a2-0410-8959-474133d486e0

--- xvidcore/src/xvid.h
+++ xvidcore/src/xvid.h
@@ -19,7 +19,7 @@
  *  along with this program ; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- * $Id: xvid.h,v 1.63 2008-11-27 20:17:33 Isibaar Exp $
+ * $Id: xvid.h,v 1.63.2.1 2008-11-28 18:28:08 Isibaar Exp $
  *
  ****************************************************************************/
 
@@ -57,10 +57,8 @@ extern "C" {
 #define XVID_API_MAJOR(a)        (((a)>>16) & 0xff)
 #define XVID_API_MINOR(a)        (((a)>> 0) & 0xff)
 
-#define XVID_VERSION             XVID_MAKE_VERSION(1,2,-127)
-#define XVID_API                 XVID_MAKE_API(4, 1)
-
-#define XVID_UNSTABLE
+#define XVID_VERSION             XVID_MAKE_VERSION(1,2,0)
+#define XVID_API                 XVID_MAKE_API(4, 2)
 
 /* Bitstream Version
  * this will be writen into the bitstream to allow easy detection of xvid
@@ -73,7 +71,7 @@ extern "C" {
  * doesnt hurt but not increasing it could cause difficulty for decoders in the
  * future
  */
-#define XVID_BS_VERSION 48
+#define XVID_BS_VERSION 49
 
 /*****************************************************************************
  * error codes
